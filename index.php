ff08882148b52cbbadfa627f8dfedc2f36e7e501
vvilpoz0304 <vvilpoz0304@g.educaand.es>
2025-01-28 14:57:46 +0100

    Ya muestra las marcas

--- index.php
+++ index.php
@@ -36,16 +36,16 @@
                             <?php
                             require_once "client/clientCoches.php";
 
-                            foreach ($marcas as $marca => $url) {
-                            ?>
-							<div class="box">
-								<a href="<?= $url ?>" class="image fit" title="ver video"><img src="images/<?= strtolower($marca) ?>.png" alt="logo <?= $marca ?>" /></a>
-								<div class="inner">
-                                   <h3><a href="modelos.php?marca=<?= $marca ?>" data-poptrox="ajax,600x400">Modelos <?= $marca ?></a></h3>
-									<a href="<?= $url ?>" class="button style2 fit" data-poptrox="youtube,800x400">Ver video <?= $marca ?></a>
-								</div>
-							</div>
-                            <?php
+                            foreach ($marcas as $marca) {
+                                ?>
+                                <div class="box">
+                                    <a href="<?= $marca["url"] ?>" class="image fit" title="ver video"><img src="img/<?= strtolower($marca["marca"]) ?>.png" alt="logo <?= $marca["marca"] ?>" /></a>
+                                    <div class="inner">
+                                        <h3><a href="modelos.php?marca=<?= $marca["marca"] ?>" data-poptrox="ajax,600x400">Modelos <?= $marca["marca"] ?></a></h3>
+                                        <a href="<?= $marca["url"] ?>" class="button style2 fit" data-poptrox="youtube,800x400">Ver video <?= $marca["marca"] ?></a>
+                                    </div>
+                                </div>
+                                <?php
                             }
                             ?>
 						</div>
